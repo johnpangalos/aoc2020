26f26fa75dc2c6e37cf8171e605349da81609569
John Pangalos <jpangs88@gmail.com>
2020-12-01 20:46:04 +0100

    Add day 1
    This is the day 01 but it's not very refined, going to look at some
    other solutions and see if there is something better.

--- src/day01.rs
+++ src/day01.rs
@@ -1,4 +1,57 @@
-
 pub fn part1(inp: String) {
-    println!("{}", inp.chars().rev().collect::<String>());
+    let mut split: Vec<&str> = inp.split("\n").collect();
+    split.remove(split.len() - 1);
+    let mut found: bool = false;
+
+    for (i, s) in split.iter().enumerate() {
+        if found {
+            break;
+        }
+
+        let num = s.parse::<i32>().unwrap();
+        let mut rest = split.clone();
+        rest.remove(i);
+        for t in rest {
+            let compare = t.parse::<i32>().unwrap();
+            if num + compare == 2020 {
+                println!("{}", num * compare);
+                found = true;
+                break;
+            }
+        }
+    }
+}
+
+pub fn part2(inp: String) {
+    let mut split: Vec<&str> = inp.split("\n").collect();
+    split.remove(split.len() - 1);
+    let mut found: bool = false;
+
+    for (i, s1) in split.iter().enumerate() {
+        if found {
+            break;
+        }
+
+        let n1 = s1.parse::<i32>().unwrap();
+        let mut rest1 = split.clone();
+        rest1.remove(i);
+        for s2 in rest1 {
+            if found {
+                break;
+            }
+            let n2 = s2.parse::<i32>().unwrap();
+
+            let mut rest2 = split.clone();
+            rest2.remove(i);
+            for s3 in rest2 {
+                let n3 = s3.parse::<i32>().unwrap();
+
+                if n1 + n2 + n3 == 2020 {
+                    println!("{}", n1 * n2 * n3);
+                    found = true;
+                    break;
+                }
+            }
+        }
+    }
 }
