72ccc154e3e9e572fce6434afbf34d75938c4123
John Pangalos <jpangs88@gmail.com>
2020-12-08 10:12:51 +0100

    Day08

---
+++ src/day08.rs
@@ -0,0 +1,111 @@
+const ACC_INST: &str = "acc";
+const NOOP_INST: &str = "nop";
+const JUMP_INST: &str = "jmp";
+
+fn parse_instruction(s: &str) -> (&str, isize) {
+    let v: Vec<_> = s.split_ascii_whitespace().collect();
+    (v[0], v[1].to_string().parse::<isize>().unwrap())
+}
+
+pub fn part1(input: String) {
+    let list: Vec<String> = input
+        .lines()
+        .collect::<Vec<_>>()
+        .iter()
+        .map(|x| x.to_string())
+        .collect();
+    println!("{}", int_comp(list).0);
+}
+
+fn change_inst(list: &mut Vec<String>, idx: usize) {
+    let temp: String;
+
+    match list[idx].contains(&JUMP_INST) {
+        true => temp = list[idx].replace(JUMP_INST, NOOP_INST),
+        false => temp = list[idx].replace(NOOP_INST, JUMP_INST),
+    };
+    list[idx] = temp;
+}
+
+fn int_comp(list: Vec<String>) -> (isize, bool) {
+    let mut acc: isize = 0;
+    let mut curr_inst: isize = 0;
+    let mut run_inst: Vec<isize> = vec![];
+    loop {
+        match run_inst.contains(&curr_inst) || curr_inst as usize == list.len() {
+            true => break,
+            false => run_inst.push(curr_inst),
+        }
+
+        match parse_instruction(&list[curr_inst as usize]) {
+            (ACC_INST, val) => acc += val,
+            (NOOP_INST, _) => (),
+            (JUMP_INST, val) => {
+                curr_inst += val;
+                continue;
+            }
+            _ => (),
+        }
+        curr_inst += 1;
+    }
+    (acc, curr_inst as usize == list.len())
+}
+
+pub fn part2(input: String) {
+    let list: Vec<String> = input
+        .lines()
+        .collect::<Vec<_>>()
+        .iter()
+        .map(|x| x.to_string())
+        .collect();
+
+    let mut v_changed: Vec<usize> = vec![];
+    loop {
+        let (idx, _) = list
+            .iter()
+            .enumerate()
+            .find(|(i, item)| {
+                (item.contains(JUMP_INST) || item.contains(NOOP_INST)) && !v_changed.contains(i)
+            })
+            .unwrap();
+
+        v_changed.push(idx);
+
+        let mut curr_list = list.clone();
+        change_inst(&mut curr_list, idx);
+        let (acc, to_break) = int_comp(curr_list);
+        if to_break {
+            println!("{}", acc);
+            break;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_CASE_1: &str = "nop +0\n\
+        acc +1\n\
+        jmp +4\n\
+        acc +3\n\
+        jmp -3\n\
+        acc -99\n\
+        acc +1\n\
+        jmp -4\n\
+        acc +6\n";
+
+    #[test]
+    fn day8_part1_case_1() {
+        println!("\n\n********************\n\n");
+        part1(TEST_CASE_1.to_owned());
+        println!("\n\n********************\n\n");
+    }
+
+    #[test]
+    fn day8_part2_case_1() {
+        println!("\n\n********************\n\n");
+        part2(TEST_CASE_1.to_owned());
+        println!("\n\n********************\n\n");
+    }
+}
